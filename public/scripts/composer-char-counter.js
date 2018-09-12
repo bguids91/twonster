a6411f92cb2960511935d3d054f4786ac89c291b
bguids91 <bguids91@gmail.com>
2018-09-12 01:39:05 +0000

    Added red characters to number when below zero

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,11 +3,20 @@ $(document).ready(function () {
 
 var textarea = $('textarea')
 
-textarea.on('keypress', function() {
-  let originalCount = 139
-  let charCount = $(this).val().length
-  console.log(originalCount - charCount)
-})
+textarea.on('input', function() {
+  let textarea = $(this)
+  let charCount = textarea.val().length;
+  let remainingChar = 140 - charCount;
+  let counter = textarea.siblings('.counter');
+  counter.text(remainingChar);
+  if (counter.text() < 1) {
+    counter.css('color', 'red')
+  } else {
+    counter.css('color', 'black')
+  }
+});
+
+textarea.on
 
 
 });
