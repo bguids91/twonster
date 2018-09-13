f4170f3a337515949764d998b719115157d4fc9e
bguids91 <bguids91@gmail.com>
2018-09-13 03:06:32 +0000

    Added form validation and alert messages

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,80 +5,51 @@
  */
 $(function () {
 
-const data = [{
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small": "https://image.flaticon.com/icons/svg/826/826599.svg",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small": "https://image.flaticon.com/icons/svg/826/826610.svg",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small": "https://image.flaticon.com/icons/svg/826/826608.svg",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
 $.ajax('/tweets').done(renderTweets);
 
 $('form#new-tweet-form').on('submit', function (e) {
   e.preventDefault();
-
   let formData = $('#tweet-text').serialize();
-  console.log(formData);
+  let text = $('#tweet-text').val();
 
-  $.ajax({
-    method:'POST',
-    url: '/tweets',
-    data: formData,
-    success: function(result){
-      console.log("The AJAX POST was successful");
-    },
-    error: function(err){
-      console.log("There was an error in posting",err);
-    }
-  }).then(function () {
-    $('#tweet-text').val('');
-    $('#char-counter').text(140);
-    $('#tweets-container').empty();
-    return $.ajax('/tweets');
-  }).then(renderTweets);
+  if(text === "" || text === null || text.length > 140) {
+    return alert("Please enter a valid twwet!")
+  } else {
+    $.ajax({
+      method:'POST',
+      url: '/tweets',
+      data: formData,
+      success: function(result){
+        console.log("The AJAX POST was successful");
+      },
+      error: function(err){
+        console.log("There was an error in posting",err);
+      }
+    }).then(function () {
+      $('#tweet-text').val('');
+      $('#char-counter').text(140);
+      $('#tweets-container').empty();
+      return $.ajax('/tweets');
+    }).then(renderTweets);
+  }
 });
 
+// function callTweetsFromMongoDb() {
 
+//   $.ajax({
+//     url: '/tweets',
+//     method: 'GET',
+//     success: function (result) {
+//       console.log("we are in success");
+//       renderTweets(result);
+//     },
+//     error: function (err) {
+//       console.log("we are in error");
+//     }
+//   });
+// }
+
+// callTweetsFromMongoDb();
 
 
 function renderTweets(tweets) {
@@ -106,8 +77,6 @@ function createTweetElement(tweet) {
   $tweet.append($header, $tweetcontent, $tweetfooter);
   return $tweet;
 }
-
-renderTweets(data);
 
 });
 
